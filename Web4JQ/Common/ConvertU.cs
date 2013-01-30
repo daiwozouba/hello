e883b686c8e58a8cd07d9dd508f3db5daa874c1a
xuelin <hexuelin@outlook.com>
2013-01-30 17:17:55 +0800

    20120130

---
+++ Web4JQ/Common/ConvertU.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Common
+{
+    public class ConvertU
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public ConvertU() { }
+
+        /// <summary>
+        /// DataTable 转为json 序列
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        public static string Dt2Json(System.Data.DataTable dt)
+        {
+            System.Web.Script.Serialization.JavaScriptSerializer jss = new System.Web.Script.Serialization.JavaScriptSerializer();
+            System.Collections.ArrayList dic = new System.Collections.ArrayList();
+            foreach (System.Data.DataRow r in dt.Rows)
+            {
+                System.Collections.Generic.Dictionary<string, string> drow = new System.Collections.Generic.Dictionary<string, string>();
+                foreach (System.Data.DataColumn col in dt.Columns)
+                {
+                    drow.Add(col.ColumnName, r[col.ColumnName].ToString());
+                }
+                dic.Add(drow);
+            }
+            return jss.Serialize(dic);
+        }
+
+        /// <summary>
+        /// DataSet 转为json 序列
+        /// </summary>
+        /// <param name="ds"></param>
+        /// <returns></returns>
+        public static string Ds2Json(System.Data.DataSet ds)
+        {
+            System.Web.Script.Serialization.JavaScriptSerializer jss = new System.Web.Script.Serialization.JavaScriptSerializer();
+            System.Collections.ArrayList dicSet = new System.Collections.ArrayList();
+            foreach (System.Data.DataTable dt in ds.Tables)
+            {
+                System.Collections.ArrayList dic = new System.Collections.ArrayList();
+                foreach (System.Data.DataRow r in dt.Rows)
+                {
+                    System.Collections.Generic.Dictionary<string, string> drow = new System.Collections.Generic.Dictionary<string, string>();
+                    foreach (System.Data.DataColumn col in dt.Columns)
+                    {
+                        drow.Add(col.ColumnName, r[col.ColumnName].ToString());
+                    }
+                    dic.Add(drow);
+                }
+                dicSet.Add(dic);
+            }
+            return jss.Serialize(dicSet);
+        }
+    }
+}
---
+++ Web4JQ/Web4Jq/Default.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using DAL;
+using Common;
+using CommLayer;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //CommLayer.Cache.Remove("");
+        Response.Cookies["ctest"].Value = "12345";
+        if ("gettbinfo" == Request.QueryString["method"])
+        {
+            int pageIndex =Convert.ToInt32(Request.QueryString["pageindex"]??"0");
+            int pageSize = Convert.ToInt32(Request.QueryString["pagesize"]??"10");
+            getDataByPage(pageIndex, pageSize);
+        }
+    }
+
+    protected void getDataByPage(int _pageIndex,int _pageSize)
+    {
+        DataSet ds = new DataSet();
+        DALBase dal = new DALBase();
+        ds = dal.GetPagingDataSet("t_Test", " 1=1", "order by d desc", " * ", _pageIndex, _pageSize);
+        DataTable dt = ds.Tables[0];
+        DataRow[] drs = dt.Select("B='b10'");
+        foreach(DataRow dr in drs)
+        {
+            dr["B"] = "X"+dr["B"].ToString();
+        }
+        Response.Write(Common.ConvertU.Ds2Json(ds));
+        Response.End();
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        Label1.Text= HttpUtility.UrlEncode(TextBox1.Text.Trim());
+    }
+}
